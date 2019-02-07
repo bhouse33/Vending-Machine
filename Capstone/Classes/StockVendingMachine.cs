212e9a334be1997a4847859fa90c8f068536f51e
Tia Smith <tia.smith@gmail.com>
2019-02-07 10:45:03 -0500

    Created VendingMachineItem class and restructed StockVendingMachine

--- Capstone/Classes/StockVendingMachine.cs
+++ Capstone/Classes/StockVendingMachine.cs
@@ -5,18 +5,36 @@ using System.Text;
 
 namespace Capstone.Classes
 {
+    /// <summary>
+    /// Represents stocking a vending machine.
+    /// </summary>
     public class StockVendingMachine
     {
-        //=new Dictionary<string, VendingMachineItem>()
-
         /// <summary>
-        /// Dictionary of vending machine items.
+        /// Holds the vending items in a dictionary.
         /// </summary>
         private Dictionary<string, VendingMachineItem> VendingMachineItems;
 
+        /// <summary>
+        /// Initializes the vending machine inventory.
+        /// </summary>
+        /// <param name="fileName"></param>
         public StockVendingMachine(string fileName)
         {
+            //Create new dictionary of vending items
             this.VendingMachineItems = new Dictionary<string, VendingMachineItem>();
+
+            //Create the items
+            PopulateVendingMachineWithInitialItems(fileName);
+  
+        }
+
+        /// <summary>
+        /// Populates vending machine with initial items.
+        /// </summary>
+        /// <param name="fileName"></param>
+        void PopulateVendingMachineWithInitialItems(string fileName)
+        {
             try
             {
                 using (StreamReader sr = new StreamReader(fileName))
@@ -27,9 +45,8 @@ namespace Capstone.Classes
                         string[] vendingItem = item.Split('|');
                         for (int i = 0; i < 4; i++)
                         {
-
-                           // VendingMachineItems.Add
-                            }
+                            VendingMachineItems.Add(vendingItem[0], new VendingMachineItem(vendingItem[0], vendingItem[1], decimal.Parse(vendingItem[2]), vendingItem[3]));
+                        }
                     }
                 }
 
@@ -40,10 +57,8 @@ namespace Capstone.Classes
             {
 
             }
+
         }
-
-
-
 
     }
 }
--- Capstone/Classes/VendingMachineItem.cs
+++ Capstone/Classes/VendingMachineItem.cs
@@ -6,9 +6,45 @@ namespace Capstone.Classes
 {
     public class VendingMachineItem
     {
+        /// <summary>
+        /// Represents the slot code
+        /// </summary>
+        public string SlotLocation;
 
+        /// <summary>
+        /// Represents the name of the product (what's on the packaging).
+        /// </summary>
+        public string ProductName;
 
+        /// <summary>
+        /// Price of the item.
+        /// </summary>
+        public decimal Price;
 
+        /// <summary>
+        /// Type of product: candy, gum, beverage, chips
+        /// </summary>
+        public string ProductType;
 
+        /// <summary>
+        /// Number of items left, starts at 5.
+        /// </summary>
+        public int Quantity;
+
+        /// <summary>
+        /// Create new Vending Machine Item.
+        /// </summary>
+        /// <param name="slotLocation"></param>
+        /// <param name="productName"></param>
+        /// <param name="price"></param>
+        /// <param name="productType"></param>
+        public VendingMachineItem(string slotLocation, string productName, decimal price, string productType)
+        {
+            this.SlotLocation = slotLocation;
+            this.ProductName = productName;
+            this.Price = price;
+            this.ProductType = productType;
+            this.Quantity = 5;
+        }
     }
 }
