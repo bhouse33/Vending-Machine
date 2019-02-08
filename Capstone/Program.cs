6da5b530ec9fb4fc265241b02c15bd3978e366b2
Tia Smith <tia.smith@gmail.com>
2019-02-08 09:21:33 -0500

    Adjusted log format

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -19,12 +19,12 @@ namespace Capstone
 
 //TO Do List
 //1** - formatting for display() - Display the "Display the Vending Machine Item" formatting fix (Brandon) - done
-//2 - Feed in only read dollar amounts ($1, $2, $20, $100, $10, $5, $50)
+//2 - Feed in only read dollar amounts ($1, $2, $20, $100, $10, $5, $50) TIA
 //3** - Code for (3) Finish Transaction - while loop (Brandon) - done
 //4 - add soldout logic
-//5 - Change wording on FeedMoney menu
-//6 - Add to upper and to lower in enter product menu
-//7 - get the log.txt to display feedmoney amounts with the $.00
+//5 - Change wording on FeedMoney menu TIA
+//6 - Add to upper and to lower in enter product menu BRANDON
+//7 - get the log.txt to display feedmoney amounts with the $.00            TIA
 //8 - add fancy images for each type of product - for dispense method
 //9 - perhaps color coded in display of items - either by row or type
 //10 - catch all exceptions for incorrect inputs
