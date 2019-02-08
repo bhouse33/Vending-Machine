61142d7f909c7abdb8ba20b935ac18e62eab4a41
Tia Smith <tia.smith@gmail.com>
2019-02-08 15:18:03 -0500

    Updated todo list

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -19,10 +19,9 @@ namespace Capstone
 }
 
 //TO Do List
-//1 - add fancy images for each type of product - for dispense method
-//2 - perhaps color coded in display of items - either by row or type
-//3 - Print Sales Report TIA
-//5 - Add existing report logic
+//1 - perhaps color coded in display of items - either by row or type
+//2 - Print Sales Report TIA
+//3 - Add existing report logic
 
 //UNIT TESTS
 //test that quantity is reduced when purchased
